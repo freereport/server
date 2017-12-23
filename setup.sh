2ea4b11073d5eb5db6b8db6a170b471437fd0d4e
freereport <rodrigoinacio@hotmail.com>
2017-12-23 18:02:41 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,17 +10,14 @@ sudo sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/s
 sudo sed -i -e 's/PrintMotd no/PrintMotd yes/g' /etc/ssh/sshd_config;
 sudo sed -i -e 's/#Banner/Banner/g' /etc/ssh/sshd_config;
 
-sudo echo 'cat /var/log/auth.log* | grep 'Failed password' | grep sshd | awk '{print $1,$2}' | sort | uniq -c' > ~/howmanyfailed.sh;
+sudo echo "cat /var/log/auth.log* | grep 'Failed password' | grep sshd | awk '{print $1,$2}" | sort | uniq -c' > ~/howmanyfailed.sh;
 sudo chmod +x ~/howmanyfailed.sh;
 
-echo $'set smooth\nset autoindent\nset tabsize 4\nset tabstospaces\nset const' > .nanorc;
+echo $'set smooth\nset autoindent\nset tabsize 4\nset tabstospaces\nset const' > ~/.nanorc;
 
-sudo echo "sudo apt update && sudo apt upgrade -y && sudo apt autoclean && sudo apt autoremove -y" > up.sh;
-sudo chmod +x ~/up.sh;
-./up.sh;
+sudo echo "sudo apt update && sudo apt upgrade -y && sudo apt autoclean && sudo apt autoremove -y" > ~/up.sh;
+sudo chmod +x ~/up.sh && ~./up.sh;
 sudo apt install nmap netcat tmux htop acpi network-manager ffmpeg tree ranger wifite
-echo "nmtiu" > wifi
-echo "acpi" > battery
-chmod +x wifi
-chmod +x battery
+echo "nmtiu" > ~/wifi && chmod +x ~/wifi;
+echo "acpi" > ~/battery && chmod +x ~/battery;
 echo "Done.";
