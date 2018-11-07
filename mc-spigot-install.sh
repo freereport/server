046631692d790484ede3b89bdad5e06975699256
freereport <rodrigoinacio@hotmail.com>
2018-11-06 20:26:03 -0500

    Create mc-spigot-install.sh

---
+++ mc-spigot-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+echo "..................................................................."
+echo "Java 9"
+sudo apt install openjdk-9-jre-headless
+echo "..................................................................."
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw allow 25565
+echo "..................................................................."
+sudo adduser minecraft
+sudo su - minecraft
+echo "..................................................................."
+mkdir /home/minecraft/build
+cd /home/minecraft/build
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar
+echo "..................................................................."
+mkdir ../server
+cd ../server
+mv ../build/spigot-1.*.jar spigot.jar
+echo "..................................................................."
