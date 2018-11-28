0ba39d3f73bc435877638fbf8871bdfbc7c89ebe
freereport <rodrigoinacio@hotmail.com>
2018-11-28 16:01:08 -0500

    Update deploy-django.sh

--- deploy-django.sh
+++ deploy-django.sh
@@ -87,8 +87,7 @@ STRINGTOREPL+="','HOST':'localhost','PORT': ''"
 echo "$STRINGTOFIND replacing with $STRINGTOREPL"
 sed -i -e 's|$STRINGTOFIND|$STRINGREPL|g' "$FILENAME"
 
-
-echo STATIC_ROOT=os.path.join(BASE_DIR,'static/') >> $FILENAME
+echo "STATIC_ROOT=os.path.join(BASE_DIR,'static/')" >> $FILENAME
 read a;
 /home/$SUDO_USER/$PROJECT/$PROJECT/python manage.py makemigrations
 /home/$SUDO_USER/$PROJECT/$PROJECT/python manage.py migrate
