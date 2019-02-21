eb8b164c4cdf7964d144267ca95d9b4072f7d41f
freereport <rodrigoinacio@hotmail.com>
2019-02-21 14:56:52 -0500

    Update wkhtmltopdf-install.sh

--- wkhtmltopdf-install.sh
+++ wkhtmltopdf-install.sh
@@ -4,8 +4,8 @@
 # run this script as root
 
 cd
-apt-get install xvfb curl -y
-apt-get -f install
+apt-get install xvfb wkhtmltopdf -y
+#apt-get -f install
 echo 'exec xvfb-run -a -s "-screen 0 640x480x16" wkhtmltopdf "$@"' | sudo tee /usr/local/bin/wkhtmltopdf.sh >/dev/null
 chmod a+x /usr/local/bin/wkhtmltopdf.sh
 
